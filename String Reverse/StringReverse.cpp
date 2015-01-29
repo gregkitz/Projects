835b27ac8de00b2b4124e0a4a24bd2dc574a88a7
GregKitz <greg.kitzmiller@gmail.com>
2015-01-29 12:43:40 -0800

    added second version

--- String Reverse/StringReverse.cpp
+++ String Reverse/StringReverse.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 void string_reverse1(char*); 
+char *string_reverse2(const char *string);
 
 int main()
 {
 	char test[] = "millerlite"; 
 	string_reverse1(test); 
 	cout << test << endl; 
+	char* test2 = string_reverse2("verycool"); 
+	cout << test2 << endl; 
 }
 
 
@@ -32,5 +35,34 @@ void string_reverse1(char *string)
 		end--;
 	}
 
+
+
+}
+
+char *string_reverse2(const char *string)
+{
+	int end = 0, beginning = 0; 
+	while (string[end] != NULL)
+	{
+		end++;
+	}
+	char *newString = new char[end]; 
+	for (int i = 0; i < end; i++)
+	{
+		newString[i] = ' '; 
+	}
+	newString[end] = '\0'; 
+	end--; 
+	char temp; 
+	while (beginning < end)
+	{
+		temp = string[beginning]; 
+		newString[beginning] = string[end]; 
+		newString[end] = temp; 
+		beginning++;
+		end--; 
+	}
+	
+	return newString; 
 
 }
